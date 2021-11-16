d70ed6d47bb4aed6b3d80567af924afedaf19ba4
KonnerCurtis <64322161+KonnerCurtis@users.noreply.github.com>
2021-11-16 10:43:06 -0700

    Update timer.h

--- src/timer.h
+++ src/timer.h
@@ -1,13 +1,17 @@
+// Author:         
+// Net ID:         
+// Date:           
+// Assignment:     Lab 3
+//----------------------------------------------------------------------//
 
 #ifndef TIMER_H
 #define TIMER_H
+
 #include <avr/io.h>
-#include<Arduino.h>
 
-void initTimer0();
 void initTimer1();
-
-void delay_Ms(unsigned int delay);
-void delay_s(unsigned int delay);
+void delayUs(unsigned int delay);
+void initTimer0();
+void delayMs(unsigned int delay); 
 
 #endif
