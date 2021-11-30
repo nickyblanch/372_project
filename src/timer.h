9ae90a2e5072e729db0055dd9c83fc8cd3914e5d
KonnerCurtis <64322161+KonnerCurtis@users.noreply.github.com>
2021-11-30 13:14:11 -0700

    Update timer.h

--- src/timer.h
+++ src/timer.h
@@ -1,15 +1,10 @@
-// Author:         
-// Net ID:         
-// Date:           
-// Assignment:     Lab 3
-//----------------------------------------------------------------------//
-
 #ifndef TIMER_H
 #define TIMER_H
 
 #include <avr/io.h>
 
 void initTimer1();
+void initTimer4();
 void delayUs(unsigned int delay);
 void initTimer0();
 void delayMs(unsigned int delay); 
