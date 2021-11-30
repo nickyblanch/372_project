1523a4cfe88693b41c3ad30261d7fb1a8f2bc176
KonnerCurtis <64322161+KonnerCurtis@users.noreply.github.com>
2021-11-30 13:12:11 -0700

    Update screamingRoboBat.cpp

--- src/screamingRoboBat.cpp
+++ src/screamingRoboBat.cpp
@@ -1,1 +1,21 @@
 
+
+#include <avr/io.h>
+#include <Arduino.h>
+//#include <util/delay.h>
+#include "timer.h"
+
+
+void initSonar(){
+    DDRD |= (1<<DDD0);//Pin 21 output to send sonar trigger pulse
+}
+void sendPulse(){//function to trigger sonar
+    PORTD |= (1<<PORTD0);
+    delayUs(10);
+    PORTD &= ~(1<<PORTD0);
+}
+int inches(int desired_inches){//converts input in inches to a rough value to compare to timer, so an object 1 inch away will trigger (roughly) when TCNT4 < 100
+    return (desired_inches * 80);
+}
+
+
